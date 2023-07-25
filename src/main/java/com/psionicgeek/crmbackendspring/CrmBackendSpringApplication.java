ef92efd27da1b3c82ed6acfed8b73579dd6b1534
PsionicGeek <rrkumar220920@gmail.com>
2023-07-25 21:07:45 +0530

    Initial Commit

---
+++ src/main/java/com/psionicgeek/crmbackendspring/CrmBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.psionicgeek.crmbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psionicgeek/crmbackendspring/CrmBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psionicgeek.crmbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
